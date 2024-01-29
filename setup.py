ffd939ea204eb277288534574b0382f0bc842668
AashishSec ఆశిష్ <65489287+aashishsec@users.noreply.github.com>
2024-01-29 09:41:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='pyWebCrawler',
+    version='1.0.2',
+    description='pyWebCrawler is a Python-based web crawling tool designed to automatically extract URLs, subdomains, and JavaScript files from websites.',
+    author='Aashishsec',
+    url='https://github.com/aashishsec/pyWebCrawler',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'colorama',
+    ],
+    extras_require={
+        'dev': ['argparse', 'concurrent.futures','re','random'],
+    },
+    entry_points={
+        'console_scripts': [
+            'pyWebCrawler = pyWebCrawler.pyWebCrawler:main',
+        ],
+    },
+)
